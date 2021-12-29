674db9afb9714b1235db1931199cd8d3ea567c22
JULIEN TABARY <julen.tabary@engie.com>
2021-12-29 13:32:10 +0100

    Give a try

--- samples-rust/basic-4dk-sample-integrated-with-rocket/src/main.rs
+++ samples-rust/basic-4dk-sample-integrated-with-rocket/src/main.rs
@@ -1,21 +1,26 @@
-#[macro_use] extern crate rocket;
+#[macro_use]
+extern crate rocket;
 extern crate dddk_core;
 
 use std::any::{Any, TypeId};
 use dddk_core::dddk::command::bus::command_dispatcher::CommandDispatcher;
 use dddk_core::dddk::command::bus::command_bus_injected::CommandBusParent;
 use dddk_core::dddk::command::command::Command;
+use dddk_core::dddk::command::command_bus::CommandBus;
 use dddk_core::dddk::command::command_handler::{CommandHandleInBus, CommandHandler};
 use dddk_core::dddk::event::event::Event;
 use rocket::State;
 
 struct ValueToInject {
-    value: bool
+    value: bool,
 }
-unsafe impl Send for ValueToInject { }
-unsafe impl Sync for ValueToInject { }
 
-struct ACommand { }
+unsafe impl Send for ValueToInject {}
+
+unsafe impl Sync for ValueToInject {}
+
+struct ACommand {}
+
 impl Command for ACommand {
     fn as_any(&self) -> &dyn Any {
         self
@@ -23,12 +28,14 @@ impl Command for ACommand {
 }
 
 struct ACommandHandler {}
+
 impl CommandHandler<ACommand> for ACommandHandler {
     fn handle<'a>(&mut self, _command: &'a ACommand) -> Vec<Box<dyn Event>> {
         println!("Has Been Called");
         return Vec::new();
     }
 }
+
 impl CommandHandleInBus for ACommandHandler {
     fn handle_from_bus<'a>(&mut self, command: &'a dyn Command) -> Vec<Box<dyn Event>> {
         return self.handle_command(command);
@@ -41,20 +48,24 @@ impl CommandHandleInBus for ACommandHandler {
 
 #[get("/")]
 fn index(value: &State<ValueToInject>) -> String {
-    let response = String::from("Hello, world! ") +  &value.value.to_string();
+    let response = String::from("Hello, world! ") + &value.value.to_string();
     return response;
 }
 
 #[rocket::main]
 async fn main() {
-    let mut command_handler = ACommandHandler {};
-    let mut command_handlers = Vec::new() as Vec<&mut dyn CommandHandleInBus>;
-    command_handlers.push(&mut command_handler);
-    let mut command_dispatcher = CommandDispatcher::new(command_handlers);
-    let mut command_bus = CommandBusParent::new(&mut command_dispatcher);
-    let value = ValueToInject { value: true };
-    let _server = rocket::build()
-        .manage(value)
-        //.manage(command_bus)
-        .mount("/", routes![index]).launch().await;
+        let mut command_handler: ACommandHandler = ACommandHandler {};
+        let mut command_handlers: Vec<&mut dyn CommandHandleInBus> = Vec::new() as Vec<&mut dyn CommandHandleInBus>;
+        let command_handler_ref: &mut ACommandHandler = &mut command_handler;
+        command_handlers.push(command_handler_ref);
+        let  mut command_dispatcher: CommandDispatcher = CommandDispatcher::new(command_handlers);
+        let mut command_bus: CommandBusParent = CommandBusParent::new(&mut command_dispatcher);
+
+        let command_bus_ref: &mut CommandBusParent = &mut command_bus;
+
+        let value = ValueToInject { value: true };
+        let _server = rocket::build()
+            .manage(value)
+            .manage(command_bus_ref)
+            .mount("/", routes![index]).launch().await;
 }
