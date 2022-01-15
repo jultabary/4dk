0203fc666913bc827a132b7ac604c95e6aaed579
Sofia Calcagno <sofia.calcagno@octo.com>
2022-01-15 13:22:13 +0100

    feat(core-rust): add error

--- project/core-rust/src/dddk/mod.rs
+++ project/core-rust/src/dddk/mod.rs
@@ -1,6 +1,9 @@
 pub mod command {
     pub mod command_bus;
     pub mod bus_impl {
+        pub mod error {
+            pub mod unknown_command_error;
+        }
         pub mod command_dispatcher;
         pub mod command_dispatcher_test;
         pub mod command_bus_shared_btw_threads;
