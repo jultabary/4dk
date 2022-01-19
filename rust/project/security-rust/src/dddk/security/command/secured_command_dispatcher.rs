b45ce5c9ca2ff553f1b4975eef520f9214884fd6
Julien TABARY <sofia.calcagno@octo.com>
2022-01-19 20:32:12 +0100

    feat(rust-security): improve SecuredCommandDispatcher implementation

--- rust/project/security-rust/src/dddk/security/command/secured_command_dispatcher.rs
+++ rust/project/security-rust/src/dddk/security/command/secured_command_dispatcher.rs
@@ -6,39 +6,83 @@ use dddk_core::dddk::command::command_bus::CommandBus;
 use dddk_core::dddk::command::command_handler::CommandHandlerInBus;
 use dddk_core::dddk::event::event::Event;
 use crate::dddk::security::authorized_strategy::AuthorizedStrategy;
+use crate::dddk::security::command::secured_command::SecuredCommand;
 use crate::dddk::security::command::secured_command_handler::SecuredCommandHandler;
 use crate::dddk::security::permission::Permission;
 
 pub struct SecuredCommandDispatcher {
-    is_command_handlers_restricted: HashMap<TypeId, Permission>,
-    command_handlers: Vec<Box<dyn CommandHandlerInBus>>,
+    secured_handler_expected_permission: HashMap<TypeId, Permission>,
+    command_handlers: HashMap<TypeId, Box<dyn CommandHandlerInBus>>,
     authorized_strategy: Box<dyn AuthorizedStrategy>,
 }
 
 impl SecuredCommandDispatcher {
-    pub fn new(command_handlers: Vec<Box<dyn CommandHandlerInBus>>, authorized_strategy: Box<dyn AuthorizedStrategy>) -> SecuredCommandDispatcher {
-        let mut is_command_handlers_restricted = HashMap::new();
-        command_handlers.iter().for_each(|command_handler| {
-           if let Some(secured_command_handler) = command_handler.as_any().downcast_ref::<SecuredCommandHandler>(){
-               let associated_command_type_id = secured_command_handler.get_associated_command_from_bus();
-               let associated_permission = secured_command_handler.get_associated_permission();
-               is_command_handlers_restricted.insert(associated_command_type_id, associated_permission);
-           }
+    pub fn new(given_command_handlers: Vec<Box<dyn CommandHandlerInBus>>, authorized_strategy: Box<dyn AuthorizedStrategy>) -> SecuredCommandDispatcher {
+        let mut secured_handler_expected_permission = HashMap::new();
+        let mut command_handlers = HashMap::new() as HashMap<TypeId, Box<dyn CommandHandlerInBus>>;
+        given_command_handlers.into_iter().for_each(|command_handler| {
+            if let Some(secured_command_handler) = command_handler.as_any().downcast_ref::<SecuredCommandHandler>() {
+                let associated_command_type_id = secured_command_handler.get_associated_command_from_bus();
+                let associated_permission = secured_command_handler.get_associated_permission();
+                secured_handler_expected_permission.insert(associated_command_type_id.clone(), associated_permission);
+                command_handlers.insert(associated_command_type_id, command_handler);
+            } else {
+                let associated_command_type_id = command_handler.get_associated_command_from_bus();
+                command_handlers.insert(associated_command_type_id, command_handler);
+            }
         });
         SecuredCommandDispatcher {
-            is_command_handlers_restricted,
+            secured_handler_expected_permission,
             command_handlers,
-            authorized_strategy
+            authorized_strategy,
         }
     }
 
     pub fn is_command_handler_restricted(&self, command_type_id: TypeId) -> bool {
-        self.is_command_handlers_restricted.get(&command_type_id).is_some()
+        self.secured_handler_expected_permission.get(&command_type_id).is_some()
+    }
+
+    pub fn get_command_handler_associated_to_command(&self, command_type_id: TypeId) -> Option<&Box<dyn CommandHandlerInBus>> {
+        self.command_handlers.get(&command_type_id)
+    }
+
+    fn get_command_handler_from_secured_command(&self, secured_command: &SecuredCommand) -> &dyn CommandHandlerInBus {
+        self.get_command_handler_from_command(secured_command.get_command())
+    }
+
+    fn get_command_handler_from_command(&self, command: &dyn Command) -> &dyn CommandHandlerInBus {
+        let command_type_id = command.as_any().type_id();
+        if let Some(command_handler) = self.command_handlers.get(&command_type_id) {
+            return command_handler.as_ref();
+        }
+        panic!("No Handler found");
+    }
+
+    fn get_handler_expected_permission(&self, secured_command: &SecuredCommand) -> Permission {
+        let command_type_id = secured_command.get_command().as_any().type_id();
+        self.secured_handler_expected_permission.get(&command_type_id).unwrap().clone()
     }
 }
 
 impl CommandBus for SecuredCommandDispatcher {
-    fn dispatch<'b>(&self, _command: &'b dyn Command) -> Vec<Arc<dyn Event>> {
-        todo!()
+    fn dispatch<'b>(&self, command: &'b dyn Command) -> Vec<Arc<dyn Event>> {
+        return if let Some(secured_command) = command.as_any().downcast_ref::<SecuredCommand>() {
+            let command_handler = self.get_command_handler_from_secured_command(secured_command);
+            let permission = self.get_handler_expected_permission(secured_command);
+            let authorization = self.authorized_strategy.is_authorized(
+                permission,
+                secured_command.get_roles_names()
+            );
+            if authorization.is_authorized() {
+                return command_handler.handle_from_bus(secured_command.get_command());
+            }
+            return Vec::new();
+        } else {
+            let command_handler = self.get_command_handler_from_command(command);
+            if let Some(_) = command_handler.as_any().downcast_ref::<SecuredCommandHandler>() {
+                return Vec::new();
+            }
+            command_handler.handle_from_bus(command)
+        };
     }
 }
--- rust/project/security-rust/src/dddk/security/command/secured_command_dispatcher_test.rs
+++ rust/project/security-rust/src/dddk/security/command/secured_command_dispatcher_test.rs
@@ -1,11 +1,17 @@
 #[cfg(test)]
 pub mod test {
     use std::any::TypeId;
+    use dddk_core::dddk::command::command_bus::CommandBus;
     use dddk_core::dddk::command::command_handler::CommandHandlerInBus;
     use crate::dddk::security::authorized_strategy_impl::no_security_strategy::NoSecurityStrategy;
+    use crate::dddk::security::authorized_strategy_impl::role_based_strategy::RoleBasedStrategy;
+    use crate::dddk::security::command::secured_command::SecuredCommand;
     use crate::dddk::security::command::secured_command_dispatcher::SecuredCommandDispatcher;
+    use crate::dddk::security::command::secured_command_handler::SecuredCommandHandler;
     use crate::dddk::security::test_tools::fake_role_repository::fake_role_repository::get_fake_repository_with_filled_with_roles;
-    use crate::dddk::security::test_tools::some_secured_command_for_test::some_secured_command_for_test::{ACommand, AnotherCommand};
+    use crate::dddk::security::test_tools::some_event_for_test::some_event_for_test::{AnEvent, AnotherEvent};
+    use crate::dddk::security::test_tools::some_role_and_permission_for_test::some_role_and_permission_for_test::{get_a_role, get_another_role};
+    use crate::dddk::security::test_tools::some_secured_command_for_test::some_secured_command_for_test::{ACommand, AnotherCommand, get_a_command_secured};
     use crate::dddk::security::test_tools::some_secured_command_handler_for_test::some_secured_command_handler_for_test::{AnotherCommandHandler, get_a_command_handler_secured};
 
     #[test]
@@ -27,4 +33,144 @@ pub mod test {
         assert_eq!(true, secured_command_dispatcher.is_command_handler_restricted(TypeId::of::<ACommand>()));
         assert_eq!(false, secured_command_dispatcher.is_command_handler_restricted(TypeId::of::<AnotherCommand>()));
     }
+
+    #[test]
+    fn it_should_return_command_handler_associated_to_its_command_when_calling_getter() {
+        // Given
+        let a_command_handler_secured = get_a_command_handler_secured();
+        let another_command_handler = AnotherCommandHandler::new();
+        let mut command_handlers = Vec::new() as Vec<Box<dyn CommandHandlerInBus>>;
+        command_handlers.push(Box::new(a_command_handler_secured));
+        command_handlers.push(Box::new(another_command_handler));
+
+        let secured_command_dispatcher = SecuredCommandDispatcher::new(
+            command_handlers,
+            Box::new(NoSecurityStrategy::new(get_fake_repository_with_filled_with_roles())),
+        );
+        // When
+        let a_command_handler_opt = secured_command_dispatcher
+            .get_command_handler_associated_to_command(TypeId::of::<ACommand>());
+        let another_command_handler_opt = secured_command_dispatcher
+            .get_command_handler_associated_to_command(TypeId::of::<AnotherCommand>());
+
+        // Then
+        assert_eq!(true, a_command_handler_opt.is_some());
+        let secured_command_handler_opt = a_command_handler_opt.
+            unwrap().as_any().downcast_ref::<SecuredCommandHandler>();
+        assert_eq!(true, secured_command_handler_opt.is_some());
+        let another_command_handler =
+            another_command_handler_opt.unwrap().as_any().downcast_ref::<AnotherCommandHandler>();
+        assert_eq!(true, another_command_handler.is_some());
+    }
+
+    #[test]
+    fn it_should_not_dispatch_unsecured_command_to_a_secured_command_handler() {
+        // Given
+        let a_command_handler_secured = get_a_command_handler_secured();
+        let mut command_handlers = Vec::new() as Vec<Box<dyn CommandHandlerInBus>>;
+        command_handlers.push(Box::new(a_command_handler_secured));
+
+        let secured_command_dispatcher = SecuredCommandDispatcher::new(
+            command_handlers,
+            Box::new(NoSecurityStrategy::new(get_fake_repository_with_filled_with_roles())),
+        );
+        let unsecured_command = ACommand::new();
+
+        // When
+        let events = secured_command_dispatcher.dispatch(&unsecured_command);
+
+        // Then
+        assert_eq!(0, events.len());
+    }
+
+    #[test]
+    fn it_should_dispatch_secured_command_to_its_secured_command_handler() {
+        // Given
+        let a_command_handler_secured = get_a_command_handler_secured();
+        let mut command_handlers = Vec::new() as Vec<Box<dyn CommandHandlerInBus>>;
+        command_handlers.push(Box::new(a_command_handler_secured));
+
+        let secured_command_dispatcher = SecuredCommandDispatcher::new(
+            command_handlers,
+            Box::new(NoSecurityStrategy::new(get_fake_repository_with_filled_with_roles())),
+        );
+        let secured_command = get_a_command_secured(vec![String::from("a_role")]);
+
+        // When
+        let events = secured_command_dispatcher.dispatch(&secured_command);
+
+        // Then
+        assert_eq!(1, events.len());
+        assert_eq!(true, events.get(0).unwrap().as_any().downcast_ref::<AnEvent>().is_some());
+    }
+
+    #[test]
+    fn it_should_dispatch_command_to_its_command_handler() {
+        // Given
+        let another_command_handler = AnotherCommandHandler::new();
+        let mut command_handlers = Vec::new() as Vec<Box<dyn CommandHandlerInBus>>;
+        command_handlers.push(Box::new(another_command_handler));
+
+        let secured_command_dispatcher = SecuredCommandDispatcher::new(
+            command_handlers,
+            Box::new(NoSecurityStrategy::new(get_fake_repository_with_filled_with_roles())),
+        );
+        let command = AnotherCommand::new();
+
+        // When
+        let events = secured_command_dispatcher.dispatch(&command);
+
+        // Then
+        assert_eq!(1, events.len());
+        assert_eq!(true, events.get(0).unwrap().as_any().downcast_ref::<AnotherEvent>().is_some());
+    }
+
+    #[test]
+    fn it_should_not_dispatch_command_when_its_roles_has_not_correct_permissions() {
+        // Given
+        let not_enough_privilege_role = get_another_role().get_name().clone();
+        let repository = get_fake_repository_with_filled_with_roles();
+        let a_command_handler_secured = get_a_command_handler_secured();
+        let another_command_handler = AnotherCommandHandler::new();
+        let mut command_handlers = Vec::new() as Vec<Box<dyn CommandHandlerInBus>>;
+        command_handlers.push(Box::new(a_command_handler_secured));
+        command_handlers.push(Box::new(another_command_handler));
+
+        let secured_command_dispatcher = SecuredCommandDispatcher::new(
+            command_handlers,
+            Box::new(RoleBasedStrategy::new(repository)),
+        );
+        let command = SecuredCommand::new(Box::new(ACommand::new()), vec![not_enough_privilege_role]);
+
+        // When
+        let events = secured_command_dispatcher.dispatch(&command);
+
+        // Then
+        assert_eq!(0, events.len());
+    }
+
+    #[test]
+    fn it_should_not_dispatch_command_when_its_roles_has_correct_permissions() {
+        // Given
+        let user_role = get_a_role().get_name().clone();
+        let repository = get_fake_repository_with_filled_with_roles();
+        let a_command_handler_secured = get_a_command_handler_secured();
+        let another_command_handler = AnotherCommandHandler::new();
+        let mut command_handlers = Vec::new() as Vec<Box<dyn CommandHandlerInBus>>;
+        command_handlers.push(Box::new(a_command_handler_secured));
+        command_handlers.push(Box::new(another_command_handler));
+
+        let secured_command_dispatcher = SecuredCommandDispatcher::new(
+            command_handlers,
+            Box::new(RoleBasedStrategy::new(repository)),
+        );
+        let command = SecuredCommand::new(Box::new(ACommand::new()), vec![user_role]);
+
+        // When
+        let events = secured_command_dispatcher.dispatch(&command);
+
+        // Then
+        assert_eq!(1, events.len());
+    }
+
 }
