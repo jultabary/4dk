a19d04383c06efe207736b48b04cc14d6632f4c7
Julien TABARY <sofia.calcagno@octo.com>
2022-01-20 09:13:34 +0100

    feat(basic-4dk-sample-integrated-with-rocket): make test safe

--- rust/samples/basic-4dk-sample-integrated-with-rocket/src/usecases/commands/create_foo_command_handler_test.rs
+++ rust/samples/basic-4dk-sample-integrated-with-rocket/src/usecases/commands/create_foo_command_handler_test.rs
@@ -1,5 +1,6 @@
 #[cfg(test)]
 mod tests {
+    use std::cell::RefCell;
     use std::rc::Rc;
     use dddk_core::dddk::command::command_handler::CommandHandler;
     use dddk_core::dddk::query::response::Response;
@@ -9,9 +10,9 @@ mod tests {
     use crate::domain::repository::FooRepository;
     use crate::usecases::commands::create_foo_command_handler::CreateFooCommand;
 
-    static mut SAVE_HAS_BEEN_CALLED: bool = false;
-
-    struct FooRepositoryFake {}
+    struct FooRepositoryFake {
+        pub save_has_been_called: RefCell<Option<bool>>,
+    }
 
     impl FooRepository for FooRepositoryFake {
         fn get_all_foo(&self) -> Vec<Box<dyn Response>> {
@@ -19,23 +20,15 @@ mod tests {
         }
 
         fn save(&self, _foo: Foo) {
-            unsafe {
-                SAVE_HAS_BEEN_CALLED = true;
-            }
-        }
-    }
-
-    fn assert_save_method_has_been_called() {
-        unsafe {
-            assert_eq!(true, SAVE_HAS_BEEN_CALLED);
+            let _value = self.save_has_been_called.borrow_mut().insert(true);
         }
     }
 
     #[test]
     fn it_should_call_repository_when_command_is_handled() {
         // Given
-        let fake_repository = Rc::new(FooRepositoryFake {});
-        let command_handler = CreateFooCommandHandler::new(fake_repository);
+        let fake_repository = Rc::new(FooRepositoryFake { save_has_been_called: RefCell::new(None) });
+        let command_handler = CreateFooCommandHandler::new(fake_repository.clone());
         let command = CreateFooCommand::new(Uuid::new_v4(), String::from("Hello World !!"));
 
         // When
@@ -43,6 +36,6 @@ mod tests {
 
         // Then
         assert_eq!(true, events.is_ok());
-        assert_save_method_has_been_called()
+        assert_eq!(true, fake_repository.save_has_been_called.borrow().unwrap());
     }
 }
