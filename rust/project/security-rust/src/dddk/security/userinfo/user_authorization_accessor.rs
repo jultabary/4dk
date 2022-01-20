021315e15e4e6f0df318b22984658e09fe362196
Julien TABARY <sofia.calcagno@octo.com>
2022-01-20 09:51:06 +0100

    feat(security-rust): update UserAuthorizationAccessor

--- rust/project/security-rust/src/dddk/security/userinfo/user_authorization_accessor.rs
+++ rust/project/security-rust/src/dddk/security/userinfo/user_authorization_accessor.rs
@@ -3,7 +3,8 @@ use crate::dddk::security::errors::Forbidden;
 use crate::dddk::security::userinfo::user::UserAuthorization;
 
 pub trait UserAuthorizationAccessor {
-    fn get_user_authorization_from_http_headers(headers: HashMap<String, String>) -> Result<UserAuthorization, Forbidden>;
+    fn get_user_authorization_from_http_headers(&self, headers: HashMap<String, String>)
+                                                -> Result<UserAuthorization, Forbidden>;
 
-    fn get_user_authorization_accessor_name() -> String;
+    fn get_user_authorization_accessor_name(&self) -> String;
 }
