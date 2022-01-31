dd09afddd6e230862af503428f30b4280db41ed6
Julien TABARY <sofia.calcagno@octo.com>
2022-01-31 15:42:22 +0100

    Add context in kafka controller

--- rust/samples/complex-4dk-sample-integrated-with-actix-and-kafka/src/infrastructure/kafka/article_review_consumer.rs
+++ rust/samples/complex-4dk-sample-integrated-with-actix-and-kafka/src/infrastructure/kafka/article_review_consumer.rs
@@ -2,11 +2,17 @@ use std::sync::Arc;
 use log::info;
 use serde::Deserialize;
 use crate::Context;
+use crate::usecases::policies::article_has_been_reviewed_event::ArticleHasBeenReviewedEvent;
 
-pub fn consume_article_review_event(message: &str, _context: &Arc<Context>) {
+pub fn consume_article_review_event(message: &str, context: &Arc<Context>) {
     let article_review = serde_json::from_str::<ArticleReview>(message).unwrap();
     info!("Received message: {:?}", article_review);
-
+    let external_event = ArticleHasBeenReviewedEvent {
+        news_paper_name: article_review.news_paper_name.clone(),
+        article_title: article_review.article_title.clone(),
+        is_validated: article_review.is_validate
+    };
+    let _commands = context.bus.dispatch_external_event(&external_event);
 }
 
 #[derive(Deserialize, Debug)]
